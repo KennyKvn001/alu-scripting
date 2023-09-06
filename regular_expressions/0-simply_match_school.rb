bd71a50df5d4bc311626360997088f7e81b7beee
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-09-06 13:20:06 +0200

    regex-01

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,1 @@
 #!/usr/bin/env ruby
-def match_school(argument)
-  regex = /School/  # Define the regular expression
-  if argument =~ regex
-    puts "The argument '#{argument}' contains the word 'School'."
-  else
-    puts "The argument '#{argument}' does not contain the word 'School'."
-  end
-end
-
-# Check the argument passed to the script
-if ARGV.length == 1
-  argument = ARGV[0]
-  match_school(argument)
-else
-  puts "Usage: ruby script.rb <argument>"
-end
