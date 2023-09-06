7f168bbc797ec4b5c3ebb84db2553733b5db02bb
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-09-06 21:18:28 +0200

    regex-02

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-#^hbt{2,5}n$
-def match_pattern(argument)
-  regex = /hbt{2,5}n/  # Define the regular expression
-  if argument =~ regex
-    puts '#{argument}'
-  else
-    puts '#{argument}'
-  end
-end
-
-# Check the argument passed to the script
-if ARGV.length == 1
-  argument = ARGV[0]
-  match_pattern(argument)
-else
-  puts "Usage: ruby script.rb <argument>"
-end
+#hbt{2,5}n
